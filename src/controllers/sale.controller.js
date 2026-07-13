bb67fbcdd7192f618228376398f54f7cf390c94c
RaildoBru <raildobruno@gmail.com>
2026-07-13 00:29:42 -0300

    Ajustes em validacoes

--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -1,4 +1,5 @@
 import SaleService from "../services/sale.service.js";
+import { validateCPF } from "../utils/document.validate.js";
 
 class SaleController {
     async getAllSales(req, res) {
@@ -10,7 +11,20 @@ class SaleController {
         }
     }
     async createSale(req, res) {
-        req.body;
+
+        if (!req.body.vehicleId || !req.body.cpf) {
+            return res.status(400).json({ error: 'vehicleId and cpf are required' });
+        }
+
+        /*
+        * Para fins de facilitar os testes, a validação do CPF não está sendo feita pela formula matemática,
+        * a validação está sendo feita por uma forma mais branda, verificando o tamanho,
+        * e por lista de CPFs inválidos.
+        */
+        if (!validateCPF(req.body.cpf)) {
+            return res.status(400).json({ error: 'Invalid CPF' });
+        }
+
         const data = {
             vehicleId: req.body.vehicleId,
             buyerCpf: req.body.cpf,
@@ -26,4 +40,3 @@ class SaleController {
 }
 
 export default new SaleController();
-
