086928e822a8efe5a9e0f0846063185401fe5cc2
RaildoBru <raildobruno@gmail.com>
2026-07-15 01:25:19 -0300

    Ajuste nos indentacao

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(express.json());
 
 
- app.use('/api-docs',
-        swaggerUi.serve,
-        swaggerUi.setup(swaggerSpec)
-    );
+app.use('/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec)
+);
 
 app.use('/api',routes);
 
